27b48dd3a41476bf5210ba8f2e25672244b3bd07
Redh00k-k <jack.frost.6581@gmail.com>
2024-01-12 09:12:16 +0900

    [Update] In Windows, implement PrintTrashBox with go

--- lib/trashbox.cpp
+++ lib/trashbox.cpp
@@ -22,19 +22,6 @@ void PrintDisplayName(IShellFolder* psf, PCUITEMID_CHILD pidl, SHGDNF uFlags, PC
     }
 }
 
-// https://devblogs.microsoft.com/oldnewthing/20110830-00/?p=9773
-void PrintDetail(IShellFolder2* psf, PCUITEMID_CHILD pidl, const SHCOLUMNID* pscid, PCTSTR pszLabel){
-    VARIANT vt;
-    HRESULT hr = psf->GetDetailsEx(pidl, pscid, &vt);
-    if (SUCCEEDED(hr)) {
-        hr = VariantChangeType(&vt, &vt, 0, VT_BSTR);
-        if (SUCCEEDED(hr)) {
-            wprintf(L"%s\t: %ls\n", pszLabel, V_BSTR(&vt));
-        }
-        VariantClear(&vt);
-    }
-}
-
 // Get shell folder
 HRESULT GetRecycleBinShellFolder(void** pRecycleBinFolder){
     IShellFolder* pDesktopFolder = NULL;
@@ -67,59 +54,6 @@ HRESULT GetRecycleBinShellFolder(void** pRecycleBinFolder){
     return hr;
 }
 
-// https://memos-by-oxalis.hatenablog.com/entry/2019/05/01/183947
-// https://www.codeproject.com/Articles/2783/How-to-programmatically-use-the-Recycle-Bin
-int PrintTrashBox() {
-    // Initialize COM
-    CoInitialize(NULL);
-
-    // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/setmode?view=msvc-160
-    _setmode(fileno(stdout), _O_U8TEXT);
-    setlocale(LC_ALL, "Japanese");
-
-    // Retrieves the IShellFolder interface for the desktop folder.
-    HRESULT hr;
-    IShellFolder2* pRecycleBinFolder;
-    hr = GetRecycleBinShellFolder((void**)&pRecycleBinFolder);
-    if (FAILED(hr)) {
-        wprintf(L"Failed to GetRecycleBinShellFolder().\n");
-        CoUninitialize();
-    }
-
-    // PKEY_Size from propkey.h
-    SHCOLUMNID PKEY_Size { { 0xB725F130, 0x47EF, 0x101A, 0xA5, 0xF1, 0x02, 0x60, 0x8C, 0x9E, 0xEB, 0xAC }, 12 };
-
-    // Enumerate items in the trashbox from pRecycleBinFolder
-    IEnumIDList* pEnum;
-    hr = pRecycleBinFolder->EnumObjects(NULL, SHCONTF_FOLDERS | SHCONTF_NONFOLDERS, &pEnum);
-    if (SUCCEEDED(hr)) {
-        LPITEMIDLIST pItemIDL;
-        while (pEnum->Next(1, &pItemIDL, NULL) == S_OK) {
-            const SHCOLUMNID SCID_OriginalLocation = { PSGUID_DISPLACED, PID_DISPLACED_FROM };
-            const SHCOLUMNID SCID_DateDeleted = { PSGUID_DISPLACED, PID_DISPLACED_DATE };
-
-            wprintf(L"\n");
-            PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_INFOLDER, TEXT("InFolder"));
-            PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_NORMAL, TEXT("Normal\t"));
-            PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_FORPARSING, TEXT("ForParsing"));
-            // PrintDetail(pRecycleBinFolder, pItemIDL, &SCID_OriginalLocation, TEXT("OriginalLocation"));
-            PrintDetail(pRecycleBinFolder, pItemIDL, &SCID_DateDeleted, TEXT("DateDeleted"));
-            PrintDetail(pRecycleBinFolder, pItemIDL, &PKEY_Size, TEXT("Size\t"));
-
-            CoTaskMemFree(pItemIDL);
-        }
-        pEnum->Release();
-    }
-    else {
-        wprintf(L"Failed to enumerate Recycle Bin items.\n");
-    }
-
-    // Release resource
-    pRecycleBinFolder->Release();
-    CoUninitialize();
-
-    return 0;
-}
 
 int RestoreItem(char *file) {
     // Initialize COM
--- utils_windows.go
+++ utils_windows.go
@@ -14,13 +14,34 @@ import (
 	"golang.org/x/sys/windows"
 )
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+	"unicode/utf16"
 )
 
 // https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shfileoperationw
 var (
-	modshell32           = syscall.NewLazyDLL("Shell32.dll")
-	procSHFileOperationW = modshell32.NewProc("SHFileOperationW")
+	modshell32                     = syscall.NewLazyDLL("Shell32.dll")
+	procSHFileOperationW           = modshell32.NewProc("SHFileOperationW")
+	procSHGetDesktopFolder         = modshell32.NewProc("SHGetDesktopFolder")
+	procSHGetSpecialFolderLocation = modshell32.NewProc("SHGetSpecialFolderLocation")
+	procSHGetDisplayName           = modshell32.NewProc("SHGetDisplayName")
+	procSHGetDataFromIDList        = modshell32.NewProc("SHGetDataFromIDListW")
+
+	modole32           = syscall.NewLazyDLL("ole32.dll")
+	procCoInitialize   = modole32.NewProc("CoInitialize")
+	procCoUninitialize = modole32.NewProc("CoUninitialize")
+	procCoTaskMemFree  = modole32.NewProc("CoTaskMemFree")
+
+	moduser32            = syscall.NewLazyDLL("User32.dll")
+	procCreatePopupMenu  = moduser32.NewProc("CreatePopupMenu")
+	procGetMenuItemCount = moduser32.NewProc("GetMenuItemCount")
 )
 
 // https://pinvoke.net/default.aspx/Enums/FileFuncFlags.html
@@ -29,6 +50,25 @@ const (
 	FO_COPY   = 0x2
 	FO_DELETE = 0x3
 	FO_RENAME = 0x4
+)
+
+// https://learn.microsoft.com/ja-jp/windows/win32/api/winuser/nf-winuser-showwindow
+const (
+	SW_HIDE            = 0x00
+	SW_SHOWNORMAL      = 0x01
+	SW_NORMAL          = 0x01
+	SW_SHOWMINIMIZED   = 0x02
+	SW_SHOWMAXIMIZED   = 0x03
+	SW_MAXIMIZE        = 0x03
+	SW_SHOWNOACTIVATE  = 0x04
+	SW_SHOW            = 0x05
+	SW_MINIMIZE        = 0x06
+	SW_SHOWMINNOACTIVE = 0x07
+	SW_SHOWNA          = 0x08
+	SW_RESTORE         = 0x09
+	SW_SHOWDEFAULT     = 0x10
+	SW_FORCEMINIMIZE   = 0x11
+	SW_MAX             = 0x11
 )
 
 // https://www.pinvoke.net/default.aspx/Enums/FILEOP_FLAGS.html
@@ -74,9 +114,401 @@ type SHFILEOPSTRUCT struct {
 	ProgressTitle        *uint16
 }
 
-func PrintTrashBoxItems() (ret int) {
-	ret = int(C.PrintTrashBox())
+type STRRET struct {
+	UType uint32
+	STRRET_Anonymous
+}
+
+type STRRET_Anonymous struct {
+	Data [32]uint64
+}
+
+type FILE_ATTRIBUTE uint32
+
+const (
+	FILE_ATTRIBUTE_INVALID               FILE_ATTRIBUTE = 0xffff_ffff // -1
+	FILE_ATTRIBUTE_READONLY              FILE_ATTRIBUTE = 0x0000_0001
+	FILE_ATTRIBUTE_HIDDEN                FILE_ATTRIBUTE = 0x0000_0002
+	FILE_ATTRIBUTE_SYSTEM                FILE_ATTRIBUTE = 0x0000_0004
+	FILE_ATTRIBUTE_DIRECTORY             FILE_ATTRIBUTE = 0x0000_0010
+	FILE_ATTRIBUTE_ARCHIVE               FILE_ATTRIBUTE = 0x0000_0020
+	FILE_ATTRIBUTE_DEVICE                FILE_ATTRIBUTE = 0x0000_0040
+	FILE_ATTRIBUTE_NORMAL                FILE_ATTRIBUTE = 0x0000_0080
+	FILE_ATTRIBUTE_TEMPORARY             FILE_ATTRIBUTE = 0x0000_0100
+	FILE_ATTRIBUTE_SPARSE_FILE           FILE_ATTRIBUTE = 0x0000_0200
+	FILE_ATTRIBUTE_REPARSE_POINT         FILE_ATTRIBUTE = 0x0000_0400
+	FILE_ATTRIBUTE_COMPRESSED            FILE_ATTRIBUTE = 0x0000_0800
+	FILE_ATTRIBUTE_OFFLINE               FILE_ATTRIBUTE = 0x0000_1000
+	FILE_ATTRIBUTE_NOT_CONTENT_INDEXED   FILE_ATTRIBUTE = 0x0000_2000
+	FILE_ATTRIBUTE_ENCRYPTED             FILE_ATTRIBUTE = 0x0000_4000
+	FILE_ATTRIBUTE_INTEGRITY_STREAM      FILE_ATTRIBUTE = 0x0000_8000
+	FILE_ATTRIBUTE_VIRTUAL               FILE_ATTRIBUTE = 0x0001_0000
+	FILE_ATTRIBUTE_NO_SCRUB_DATA         FILE_ATTRIBUTE = 0x0002_0000
+	FILE_ATTRIBUTE_EA                    FILE_ATTRIBUTE = 0x0004_0000
+	FILE_ATTRIBUTE_PINNED                FILE_ATTRIBUTE = 0x0008_0000
+	FILE_ATTRIBUTE_UNPINNED              FILE_ATTRIBUTE = 0x0010_0000
+	FILE_ATTRIBUTE_RECALL_ON_OPEN        FILE_ATTRIBUTE = 0x0004_0000
+	FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS FILE_ATTRIBUTE = 0x0040_0000
+)
+
+// https://docs.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-filetime
+type FILETIME struct {
+	dwLowDateTime  uint32
+	dwHighDateTime uint32
+}
+
+// https://learn.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-win32_find_dataw
+type WIN32_FIND_DATA struct {
+	DwFileAttributes    FILE_ATTRIBUTE
+	FtCreationTime      FILETIME
+	FtLastAccessTime    FILETIME
+	FtLastWriteTime     FILETIME
+	NFileSizeHigh       uint32
+	NFileSizeLow        uint32
+	dwReserved0         uint32
+	dwReserved1         uint32
+	cFileName           [260]uint16 // MAX_PATH
+	cCAlternateFileName [14]uint16
+	DwFileType          uint32
+	DwCreatorType       uint32
+	WFinderFlags        uint16
+}
+
+const (
+	CSIDL_BITBUCKET     = 0xa
+	SHGDN_NORMAL        = 0x0000
+	SHGDN_INFOLDER      = 0x1
+	SHGDN_FOREDITING    = 0x1000
+	SHGDN_FORADDRESSBAR = 0x4000
+	SHGDN_FORPARSING    = 0x8000
+)
+
+type IShellFolderVtbl struct {
+	QueryInterface   uintptr
+	AddRef           uintptr
+	Release          uintptr
+	ParseDisplayName uintptr
+	EnumObjects      uintptr
+	BindToObject     uintptr
+	BindToStorage    uintptr
+	CompareIDs       uintptr
+	CreateViewObject uintptr
+	GetAttributesOf  uintptr
+	GetUIObjectOf    uintptr
+	GetDisplayNameOf uintptr
+	SetNameOf        uintptr
+}
+
+type IShellFolder struct {
+	lpVtbl *IShellFolderVtbl
+}
+
+type IEnumIDListVtbl struct {
+	QueryInterface uintptr
+	AddRef         uintptr
+	Release        uintptr
+	Next           uintptr
+	Skip           uintptr
+	Reset          uintptr
+	Clone          uintptr
+}
+
+type IEnumIDList struct {
+	lpVtbl *IEnumIDListVtbl
+}
+
+const (
+	SHCONTF_FOLDERS    = 0x0020
+	SHCONTF_NONFOLDERS = 0x0040
+)
+
+type IID_IShellFolder struct {
+	Data1 uint32
+	Data2 uint16
+	Data3 uint16
+	Data4 [8]byte
+}
+
+// https://docs.microsoft.com/en-us/windows/win32/api/shtypes/ns-shtypes-itemidlist
+type ITEMIDLIST struct {
+	ID SHITEMID
+}
+
+// https://docs.microsoft.com/en-us/windows/win32/api/shtypes/ns-shtypes-shitemid
+type SHITEMID struct {
+	CB   uint16
+	ABID [1]byte
+}
+
+func (v *IShellFolder) Release() int32 {
+	ret, _, _ := syscall.Syscall(
+		v.lpVtbl.Release,
+		1,
+		uintptr(unsafe.Pointer(v)),
+		0,
+		0)
+	return int32(ret)
+}
+
+func (v *IEnumIDList) Release() int32 {
+	ret, _, _ := syscall.Syscall(
+		v.lpVtbl.Release,
+		1,
+		uintptr(unsafe.Pointer(v)),
+		0,
+		0)
+	return int32(ret)
+}
+
+func (v *IShellFolder) BindToObject(pidl uintptr, pbc uintptr, riid *syscall.GUID, ppv **IShellFolder) uintptr {
+	ret, _, _ := syscall.Syscall6(
+		v.lpVtbl.BindToObject,
+		5,
+		uintptr(unsafe.Pointer(v)),
+		uintptr(unsafe.Pointer(pidl)),
+		uintptr(pbc),
+		uintptr(unsafe.Pointer(riid)),
+		uintptr(unsafe.Pointer(ppv)),
+		0)
+	return ret
+}
+
+// https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-ishellfolder-enumobjects
+func (v *IShellFolder) EnumObjects(hwnd uintptr, grfFlags int, ppenumIDlist **IEnumIDList) uintptr {
+	ret, _, _ := syscall.Syscall6(
+		v.lpVtbl.EnumObjects,
+		4,
+		uintptr(unsafe.Pointer(v)),
+		uintptr(unsafe.Pointer(hwnd)),
+		uintptr(grfFlags),
+		uintptr(unsafe.Pointer(ppenumIDlist)),
+		0,
+		0)
+	return ret
+}
+
+func (v *IEnumIDList) Next(celt int, rgelt **ITEMIDLIST, pceltFetched *uint32) uintptr {
+	ret, _, _ := syscall.Syscall6(
+		v.lpVtbl.Next,
+		4,
+		uintptr(unsafe.Pointer(v)),
+		uintptr(celt),
+		uintptr(unsafe.Pointer(rgelt)),
+		uintptr(unsafe.Pointer(pceltFetched)),
+		0,
+		0)
+	return ret
+}
+
+func (v *IShellFolder) GetDisplayNameOf(pidl *ITEMIDLIST, uFlags uint32, pName *STRRET) uintptr {
+	ret, _, _ := syscall.Syscall6(
+		v.lpVtbl.GetDisplayNameOf,
+		4,
+		uintptr(unsafe.Pointer(v)),
+		uintptr(unsafe.Pointer(pidl)),
+		uintptr(uFlags),
+		uintptr(unsafe.Pointer(pName)),
+		0,
+		0)
+	return ret
+}
+
+func CoTaskMemFree(pv uintptr) {
+	modole32.NewProc("CoTaskMemFree").Call(pv)
+}
+
+func (this *STRRET) pOleStr() **uint16 {
+	return (**uint16)(unsafe.Pointer(&this.Data[0]))
+}
+func (this *STRRET) uOffset() *uint32 {
+	return (*uint32)(unsafe.Pointer(&this.Data[0]))
+}
+func (this *STRRET) cStr() *[260]byte {
+	return (*[260]byte)(unsafe.Pointer(&this.Data[0]))
+}
+
+// https://stackoverflow.com/questions/39961171/golang-winapi-call-with-struct-parameter
+// utf16PtrToString is like UTF16ToString, but takes *uint16
+// as a parameter instead of []uint16.
+// max is how many times p can be advanced looking for the null terminator.
+// If max is hit, the string is truncated at that point.
+func utf16PtrToString(p *uint16, max int) string {
+	if p == nil {
+		return ""
+	}
+	// Find NUL terminator.
+	end := unsafe.Pointer(p)
+	n := 0
+	for *(*uint16)(end) != 0 && n < max {
+		end = unsafe.Pointer(uintptr(end) + unsafe.Sizeof(*p))
+		n++
+	}
+	s := (*[(1 << 30) - 1]uint16)(unsafe.Pointer(p))[:n:n]
+	return string(utf16.Decode(s))
+}
+
+func CStringToString(cs *uint16) (s string) {
+	if cs != nil {
+		us := make([]uint16, 0, 256)
+		for p := uintptr(unsafe.Pointer(cs)); ; p += 2 {
+			u := *(*uint16)(unsafe.Pointer(p))
+			if u == 0 {
+				return string(utf16.Decode(us))
+			}
+			us = append(us, u)
+		}
+	}
+	return ""
+}
+
+func PrintDisplayName(psf *IShellFolder, pidl *ITEMIDLIST, uFlags uint32, label string) {
+	var pName STRRET
+	ret := psf.GetDisplayNameOf(pidl, uFlags, &pName)
+	if ret != 0 {
+		fmt.Println("Failed to get item name.")
+		return
+	}
+
+	if strings.Contains(label, "DateDelete") || strings.Contains(label, "Size") {
+		recycleDir := filepath.Dir(CStringToString(*pName.pOleStr()))
+		ipath := strings.Replace(filepath.Base(CStringToString(*pName.pOleStr())), "$R", "$I", 1)
+
+		// Version 2 (Introduced somewhere in a Windows 10 release)
+		// https://github.com/danielmarschall/recyclebinunit/blob/master/FORMAT.md#version-2-introduced-somewhere-in-a-windows-10-release
+		buf := make([]byte, 24)
+		f, _ := os.Open(recycleDir + "\\" + ipath)
+		f.Read(buf)
+
+		if strings.Contains(label, "DateDelete") {
+			var dd int64
+			binary.Read(bytes.NewReader(buf[16:]), binary.LittleEndian, &dd)
+			// Convert NT time to Unix epoch
+			// Unix: 1/1/1970 00:00, Windows NT: 1/1/1601 00:00
+			ticks := time.Unix((dd/10000000)-60*60*24*365*369, 0)
+			fmt.Printf("%s\t: %v\n", label, ticks.Local())
+		} else if strings.Contains(label, "Size") {
+			var fsize int64
+			binary.Read(bytes.NewReader(buf[8:16]), binary.LittleEndian, &fsize)
+			fmt.Printf("%s\t: %v\n", label, fsize)
+		}
+	} else {
+		fmt.Printf("%s\t: %v\n", label, CStringToString(*pName.pOleStr()))
+	}
+}
+
+func _SHGetSpecialFolderLocation(
+	hwnd uintptr,
+	csidl int,
+	ppidl uintptr,
+) (r1 uintptr, err error) {
+	r1, _, err = procSHGetSpecialFolderLocation.Call(hwnd, uintptr(csidl), ppidl)
 	return
+}
+
+func _SHGetDesktopFolder(
+	ppshf uintptr,
+) (r1 uintptr, err error) {
+	r1, _, err = procSHGetDesktopFolder.Call(ppshf)
+
+	return
+}
+
+func _CoTaskMemFree(
+	pv uintptr,
+) (r1 uintptr, err error) {
+	r1, _, err = procCoTaskMemFree.Call(pv)
+
+	return
+}
+
+func _CoUninitialize() (r1 uintptr, err error) {
+	r1, _, err = procCoUninitialize.Call()
+
+	return
+}
+
+func _CoInitialize(
+	pvReserved uintptr,
+) (r1 uintptr, err error) {
+	r1, _, err = procCoInitialize.Call(pvReserved)
+
+	return
+}
+
+func GetRecycleBinShellFolder(pRecycleBinFolder **IShellFolder) (ret uintptr, err error) {
+	var pDesktopFolder *IShellFolder
+	ret, err = _SHGetDesktopFolder(uintptr(unsafe.Pointer(&pDesktopFolder)))
+	if ret != 0 {
+		return ret, err
+	}
+	defer pDesktopFolder.Release()
+
+	var pRecycleBinIDL uintptr
+	ret, err = _SHGetSpecialFolderLocation(uintptr(0), CSIDL_BITBUCKET, uintptr(unsafe.Pointer(&pRecycleBinIDL)))
+	if ret != 0 {
+		return ret, err
+	}
+
+	var IID_IShellFolder = syscall.GUID{0x000214E6, 0x0000, 0x0000, [8]byte{0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46}}
+	ret = pDesktopFolder.BindToObject(pRecycleBinIDL, uintptr(0), &IID_IShellFolder, &(*pRecycleBinFolder))
+
+	return ret, err
+}
+
+func PrintTrashBoxItems() (r int) {
+	ret, _ := _CoInitialize(uintptr(0))
+	if ret != 0 {
+		// Call FormatMessage API to display correct errors.
+		return 1
+	}
+
+	var pRecycleBinFolder *IShellFolder
+	ret, _ = GetRecycleBinShellFolder(&pRecycleBinFolder)
+	if ret != 0 {
+		return 1
+	}
+	defer pRecycleBinFolder.Release()
+
+	var pEnum *IEnumIDList
+	ret = pRecycleBinFolder.EnumObjects(0, SHCONTF_FOLDERS|SHCONTF_NONFOLDERS, &pEnum)
+	if ret != 0 {
+		fmt.Println("Failed to enumerate Recycle Bin items.")
+		return 1
+	}
+	defer pEnum.Release()
+
+	var pItemIDL *ITEMIDLIST
+	for {
+		ret = pEnum.Next(1, &pItemIDL, nil)
+		if ret != 0 {
+			break
+		}
+
+		fmt.Println()
+		PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_INFOLDER, "InFolder")
+		PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_NORMAL, "Normal\t")
+		PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_FORPARSING, "ForParsing")
+		PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_FORPARSING, "DateDeleted")
+		PrintDisplayName(pRecycleBinFolder, pItemIDL, SHGDN_FORPARSING, "Size\t")
+
+		CoTaskMemFree(uintptr(unsafe.Pointer(pItemIDL)))
+	}
+
+	_CoUninitialize()
+	return 0
+}
+
+type IContextMenuVtbl struct {
+	QueryInterface   uintptr
+	AddRef           uintptr
+	Release          uintptr
+	QueryContextMenu uintptr
+	InvokeCommand    uintptr
+	GetCommandString uintptr
+}
+
+type IContextMenu struct {
+	lpVtbl *IContextMenuVtbl
 }
 
 func RestoreItem(file string) (ret int) {
