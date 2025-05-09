b4511a669f9a79a6a69db489ac060c4cd1eaf134
Redh00k-k <jack.frost.6581@gmail.com>
2025-05-09 15:47:34 +0900

    [add] add GetTrashBoxItems for Linux

--- utils_linux.go
+++ utils_linux.go
@@ -19,6 +19,14 @@ type Info struct {
 	deletionDate time.Time
 }
 
+type fileInfoForLinux struct {
+	originalPath      string
+	trashboxFilesPath string
+	trashboxInfoPath  string
+	dateDelete        string
+	size              int64
+}
+
 func parseLine(line string, delimiter string) []string {
 	pl := strings.Split(line, delimiter)
 	return pl
@@ -35,6 +43,76 @@ func decodeLine(pl []string) string {
 
 func printDisplayName(line string, label string) {
 	fmt.Printf("%s\t: %s\n", label, line)
+}
+
+func GetTrashBoxItems() ([]fi, error) {
+	user, err := user.Current()
+	if err != nil {
+		fmt.Errorf("Failure to get user's home directory: %s", err)
+		return nil, err
+	}
+
+	// Contents of a trash directory
+	// https://specifications.freedesktop.org/trash-spec/trashspec-1.0.html
+	trashBase := strings.Replace("~/.local/share/Trash", "~", user.HomeDir, 1)
+
+	// Generate fullPath from .~/.local/share/Trash/files/
+	allFiles, err := ioutil.ReadDir(trashBase + "/files/")
+	if err != nil {
+		fmt.Errorf("Failure to get files in ~/.local/share/Trash : %s", err)
+		return nil, err
+	}
+
+	var files []fi
+	for _, file := range allFiles {
+		infoFilePath := trashBase + "/info/" + file.Name() + ".trashinfo"
+		filesFilePath := trashBase + "/files/" + file.Name()
+
+		iFile, err := os.Open(infoFilePath)
+		if err != nil {
+			fmt.Printf("Failure to open info file: %s\n", err)
+			continue
+		}
+		defer iFile.Close()
+
+		fFile, err := os.Open(filesFilePath)
+		if err != nil {
+			fmt.Printf("Failure to open files file: %s\n", err)
+			continue
+		}
+		defer fFile.Close()
+
+		var decodedFilePath string
+		var deletedDate string
+		// Read one line at a time, as the order of 'Path' and 'DeletionDate' may be different
+		scanner := bufio.NewScanner(iFile)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if pl := parseLine(line, "Path="); len(pl) > 1 {
+				decodedFilePath, _ = url.QueryUnescape(pl[1])
+			} else if pl := parseLine(line, "DeletionDate="); len(pl) > 1 {
+				deletedDate = pl[1]
+			} else {
+				// "[Trash Info]"
+				continue
+			}
+		}
+
+		fs, err := fFile.Stat()
+		if err != nil {
+			fmt.Printf("Failure to open file: %s\n", err)
+		}
+
+		var file fi
+
+		file.InFolder = filepath.Base(decodedFilePath)
+		file.Normal = decodedFilePath
+		file.DateDeleted, _ = time.Parse("2006-01-02T15:04:05Z07:00", deletedDate)
+		file.Size = fs.Size()
+		files = append(files, file)
+	}
+
+	return files, nil
 }
 
 func PrintTrashBoxItems() (ret error) {
@@ -137,15 +215,7 @@ func MoveToTrashBox(path string) (err error) {
 	return nil
 }
 
-type fileInfo struct {
-	originalPath      string
-	trashboxFilesPath string
-	trashboxInfoPath  string
-	dateDelete        string
-	size              int64
-}
-
-func addMatchedFileList(fl []fileInfo, infoFilePath string, filesFilePath string) []fileInfo {
+func addMatchedFileList(fl []fileInfoForLinux, infoFilePath string, filesFilePath string) []fileInfoForLinux {
 	iFile, err := os.Open(infoFilePath)
 	if err != nil {
 		fmt.Printf("Failure to open file: %s", err)
@@ -181,7 +251,7 @@ func addMatchedFileList(fl []fileInfo, infoFilePath string, filesFilePath string
 	}
 
 	// assign value to fileInfo
-	var fi fileInfo
+	var fi fileInfoForLinux
 	fi.originalPath = decodedFilePath
 	fi.trashboxFilesPath = filesFilePath
 	fi.trashboxInfoPath = infoFilePath
@@ -191,7 +261,7 @@ func addMatchedFileList(fl []fileInfo, infoFilePath string, filesFilePath string
 	return append(fl, fi)
 }
 
-func unDelete(id uint, fl []fileInfo, outputPath string) (err error) {
+func unDelete(id uint, fl []fileInfoForLinux, outputPath string) (err error) {
 	if uint(len(fl)) <= id {
 		return fmt.Errorf("Index out of range")
 	}
@@ -233,7 +303,7 @@ func RestoreItem(filename string, outputPath string) (ret error) {
 		return err
 	}
 
-	var fl []fileInfo
+	var fl []fileInfoForLinux
 	for _, file := range allFiles {
 		if !strings.Contains(file.Name(), filename) {
 			continue
